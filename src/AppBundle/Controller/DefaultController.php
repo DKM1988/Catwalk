e27c4df46aa9a7ae3088fa6c7b20e6e65dfc9c57
Keenan Molver <keenan.m@afrihost.com>
2016-05-14 17:25:29 +0200

    created route to admin from home

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,9 +15,21 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/login.html.twig', [
+        return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/login", name="admin_login")
+     */
+    public function adminLogin()
+    {
+        $templating = $this->container->get("templating");
+        $html = $templating->render('default/login.html.twig');
+
+        return new Response($html);
+
     }
     
 }
