4b807dbc51f75e1af491fae857c2ee071f9e23fa
Keenan Molver <keenan.m@afrihost.com>
2016-05-17 20:14:23 +0200

    moved index css to resource

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class SecurityController extends Controller
@@ -11,6 +13,33 @@ class SecurityController extends Controller
     /**
      * @Route("/login", name="login")
      */
+    public function login(Request $request)
+    {
+        $session = $request->getSession();
+
+        if ($request->attributes->has(Security::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(Security::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $request->get(Security::AUTHENTICATION_ERROR);
+            $session->remove(Security::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render('security/login.html.twig',
+            array(
+                'last_username' => $session->get(Security::LAST_USERNAME),
+                'error' => $error,
+                )
+        );
+
+
+    }
+
+
+
+
+
+
     public function loginAction(Request $request)
     {
         $authenticationUtils = $this->get('security.authentication_utils');
