a5a5f21f50aa2c3f89c3e30556051de282092843
Keenan Molver <keenan.m@afrihost.com>
2016-05-18 22:32:57 +0200

    added registration controller

--- src/AppBundle/Entity/USer.php
+++ src/AppBundle/Entity/USer.php
@@ -3,8 +3,9 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
-
 
 /**
  * Class user
@@ -56,9 +57,26 @@ class User implements UserInterface, \Serializable
      */
     private $isAdmin;
 
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(max=4096)
+     */
+    private $plainPassword;
+
+
     public function __construct()
     {
         $this->isActive = true;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail()
+    {
+        $this->setEmail();
     }
 
     public function getUsername()
@@ -66,9 +84,19 @@ class User implements UserInterface, \Serializable
         $this->getUsername();
     }
 
-    public function getSalt()
+    public function setUsername($username)
     {
-        return null;
+        $this->username = $username;
+    }
+
+    public function getPlainPassword()
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword($password)
+    {
+        $this->plainPassword = $password;
     }
 
     public function getPassword()
@@ -84,6 +112,11 @@ class User implements UserInterface, \Serializable
     public function eraseCredentials()
     {
         // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getSalt()
+    {
+        return null;
     }
 
     /** @see \Serializable::serialize() */
---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\LoginForm;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('username', TextType::class)
+            ->add('plainPassword', RepeatedType::class)
+            ->add('first_name', TextType::class)
+            ->add('last_name', TextType::class)
+            ->add('isAdmin', AbstractType::class, array(
+                    'type' => PasswordType::class,
+                    'first_options' => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Repeat Password'),
+                )
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        )
+        );
+    }
+}
